b64db6d1fbb24b5ea82d6ab2acbdb197e85a5086
jemyzhang <jemy.zhang@gmail.com>
2018-01-09 17:01:24 +0800

    change to configs repo, add auto install scripts

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -ex
+
+CURRENT_DIR=$(realpath $(dirname $(readlink -f ${BASH_SOURCE[0]:-$0})))
+
+pushd $CURRENT_DIR > /dev/null 2>&1
+
+git submodule init
+git submodule update --remote --recursive
+
+# zsh
+if [ -d $HOME/.oh-my-zsh ]; then
+  mv $HOME/.oh-my-zsh $HOME/.oh-my-zsh.old
+fi
+
+ln -s $CURRENT_DIR/oh-my-zsh $HOME/.oh-my-zsh
+
+if [ -f $HOME/.zshrc ]; then
+  mv $HOME/.zshrc $HOME/.zshrc.old
+fi
+
+ln -s $CURRENT_DIR/zshrc $HOME/.zshrc
+
+# vi
+if [ -e $HOME/.vimrc ]; then
+  mv $HOME/.vimrc $HOME/.vimrc.old
+fi
+
+ln -sf $CURRENT_DIR/vimrc $HOME/.vimrc
+
+# tmux
+if [ -e $HOME/.tmux.conf ]; then
+  mv $HOME/.tmux.conf $HOME/.tmux.conf.old
+fi
+
+ln -sf $CURRENT_DIR/tmux.conf $HOME/.tmux.conf
+
+# font
+$CURRENT_DIR/fonts/install.sh
+
+# git
+source $CURRENT_DIR/gitconfig.sh
+
+if [ ! -e $HOME/.gitignore_global ]; then
+  ln -sf $CURRENT_DIR/gitignore_global $HOME/.gitignore_global
+fi
+
+
+popd > /dev/null 2>&1
